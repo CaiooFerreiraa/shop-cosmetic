39524c7e25a74b6f090033475c4d5e4e573e79ec
CaiooFerreiaa <caioferreiraadev@gmail.com>
2025-05-24 13:35:55 -0300

    Update: Atualizando a loja com os itens

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,22 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        <img src="src\assets\logo-web-flux.jpg" alt="logo-webflux" />
+      </header>
+      <div className='cardMain'>
+        <div className='card'>
+          <img src="src\assets\dpx-logo.jpg" alt="dpx-logo"/>
+
+          <a href="https://site-dpx-wzsh.onrender.com/">Visualizar</a>
+        </div>
+
+        <div className='card'>
+          <img src="src\assets\logo-consulta.png" alt="consulta" />
+
+           <a href="http://192.168.1.2:8080">Visualizar</a>
+        </div>
       </div>
-      <h1>Aqui será nosso humilde site</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
